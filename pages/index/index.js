2e6cc75f491917719158033e518444f2d07bb75e
sirius <sirius@cqupt>
2019-04-08 22:12:27 +0800

    Initial Commit

---
+++ pages/index/index.js
@@ -0,0 +1,48 @@
+//index.js
+//获取应用实例
+const app = getApp()
+
+Page({
+  data: {
+    motto: 'Hello World',
+    userInfo: {},
+    hasUserInfo: false,
+    canIUse: wx.canIUse('button.open-type.getUserInfo')
+  },
+  onLoad: function () {
+    if (app.globalData.userInfo) {
+      this.setData({
+        userInfo: app.globalData.userInfo,
+        hasUserInfo: true
+      })
+    } else if (this.data.canIUse){
+      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
+      // 所以此处加入 callback 以防止这种情况
+      app.userInfoReadyCallback = res => {
+        this.setData({
+          userInfo: res.userInfo,
+          hasUserInfo: true
+        })
+      }
+    } else {
+      // 在没有 open-type=getUserInfo 版本的兼容处理
+      wx.getUserInfo({
+        success: res => {
+          app.globalData.userInfo = res.userInfo
+          this.setData({
+            userInfo: res.userInfo,
+            hasUserInfo: true
+          })
+        }
+      })
+    }
+  },
+  click(event){
+    var id = event.currentTarget.dataset.id;
+    if(id=='1'){
+      this.setData({ motto: '二叉树查找树的平均深度为O(logN)' });
+    }else if(id == '2'){
+      this.setData({ motto: 'N个节点的二叉树都需要N+1个null链' });
+    }
+  }
+})
